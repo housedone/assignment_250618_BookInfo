f0960ff59391bf4404803d647e72cb1e4fc56678
Wooseong <thehousedone@gmail.com>
2025-06-18 17:19:10 +0900

    feat: 필수 레벨 1 완료

--- assignment_250618_BookInfo/DataService.swift
+++ assignment_250618_BookInfo/DataService.swift
@@ -15,10 +15,6 @@ struct BookData: Codable {
     let attributes: Book
 }
 
-struct Chapter: Codable {
-    var title: String
-}
-
 struct Book: Codable {
     var title: String
     var author: String
@@ -30,6 +26,10 @@ struct Book: Codable {
     var chapters: [Chapter]
 }
 
+struct Chapter: Codable {
+    var title: String
+}
+
 class DataService {
     
     enum DataError: Error {
@@ -37,25 +37,20 @@ class DataService {
         case parsingFailed
     }
     
-    /// 책을 불러오는 함수.
-    /// completion : 클로저 파라미터. 결과가 준비되었을 때 호출된다.
-    /// @escaping : 이 클로저는 함수가 끝난 뒤에도 호출될 수 있기 때문에 escape 가능성을 명시한다.
-    /// 성공하면 [Book] / 실패하면 Error 타입을 리턴한다.
+    /// 책을 불러오는 함수. 성공하면 [Book], 실패하면 Error 타입 리턴
     func loadBooks(completion: @escaping (Result<[Book], Error>) -> Void) {
+        // completion : 클로저 파라미터. 결과가 준비되었을 때 호출된다.
+        // @escaping : 이 클로저는 함수가 끝난 뒤에도 호출될 수 있기 때문에 escape 가능성을 명시한다.
         
-        /// 앱의 번들에서 data.json이라는 파일을 찾는다.
-        /// guard let : 파일 경로가 없으면 fileNotFound라는 DataError 호출. 찾았으면 넘어감
+        // 앱의 번들에서 data.json이라는 파일을 찾는다.
         guard let path = Bundle.main.path(forResource: "data", ofType: "json") else {
             completion(.failure(DataError.fileNotFound))
             return
         }
         
-        /// 파일 경로를 URL로 변환해서 해당 파일의 내용을 Data 타입으로 읽어온다. 를 try
-        /// JSONDecoder()를 사용해 JSON 데이터를 BookResponse로 디코딩한다. BookResponse는 Codable 또는 최소 Decodable을 채택한 타입이어야 한다.
-        /// a
         do {
-            let data = try Data(contentsOf: URL(fileURLWithPath: path))
-            let bookResponse = try JSONDecoder().decode(BookResponse.self, from: data)
+            let data = try Data(contentsOf: URL(fileURLWithPath: path))                // 파일 경로를 URL로 변환해서 해당 파일의 내용을 Data 타입으로 읽어온다.
+            let bookResponse = try JSONDecoder().decode(BookResponse.self, from: data) // JSONDecoder()를 사용해 JSON 데이터를 BookResponse로 디코딩한다.
             let books = bookResponse.data.map { $0.attributes }
             completion(.success(books))
         } catch {
@@ -63,25 +58,4 @@ class DataService {
             completion(.failure(DataError.parsingFailed))
         }
     }
-    
-    private let dataService = DataService()
-
-    func loadBooks() {
-        dataService.loadBooks { [weak self] result in
-            guard let self = self else { return }
-            
-            switch result {
-            case .success(let books):
-                for book in books {
-                    
-                }
-                
-            case .failure(let error):
-                print("🚨 에러 : \(error)")
-            }
-        }
-    }
 }
-
-
-
--- assignment_250618_BookInfo/ViewController.swift
+++ assignment_250618_BookInfo/ViewController.swift
@@ -11,19 +11,74 @@ import SnapKit
 class ViewController: UIViewController {
     
     private var titleLabel = UILabel()
-
+    private var numberButton = UIButton()
+    private var bookList: [Book] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        
         configureUI()
     }
     
+    // 기본적으로 0번째 항목의 내용을 출력. 6번째까지 있음 (+= 1 하자)
+    // 버튼을 누르면 출력되는 내용이 달라져야 함
+    
     private func configureUI() {
+        loadBooks()
         
+        view.backgroundColor = .systemBackground
+        
+        titleLabel.text = bookList[1 - 1].title
+        titleLabel.font = .systemFont(ofSize: 24, weight: .bold)
+        titleLabel.textAlignment = .center
+        titleLabel.numberOfLines = 2
+        
+        numberButton.setTitle("1", for: .normal)
+        numberButton.setTitleColor(.white, for: .normal)
+        numberButton.titleLabel?.font = .systemFont(ofSize: 16)
+        numberButton.backgroundColor = .systemBlue
+        numberButton.layer.cornerRadius = 20
+        
+        
+        
+        [titleLabel, numberButton]
+            .forEach {
+                view.addSubview($0)
+            }
+        
+        // leading, trailing = superView로부터 20
+        // top = safeArea로부터 10
+        titleLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(300)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(10)
+        }
+        
+        numberButton.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(16)
+            $0.centerX.equalToSuperview()
+            $0.width.height.equalTo(40)
+            
+        }
     }
-
-
+    
+    private let dataService = DataService()
+    
+    func loadBooks() {
+        dataService.loadBooks { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let books):
+                bookList = books
+                print("책 정보 불러왔음")
+            case .failure(let error):
+                print("🚨 에러 : \(error)")
+            }
+        }
+    }
 }
 
 
