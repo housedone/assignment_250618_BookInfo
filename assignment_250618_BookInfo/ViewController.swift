c07621ae978d7758edeea452fe9f1248f0d0de81
Wooseong <thehousedone@gmail.com>
2025-06-18 22:08:01 +0900

    feat: 필수 레벨 2 기능 중 Json 파싱 실패 시 Alert 창 띄우기 제외하고 완료

--- assignment_250618_BookInfo/ViewController.swift
+++ assignment_250618_BookInfo/ViewController.swift
@@ -10,10 +10,6 @@ import SnapKit
 
 class ViewController: UIViewController {
     
-    private var titleLabel = UILabel()
-    private var numberButton = UIButton()
-    private var bookList: [Book] = []
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -22,47 +18,7 @@ class ViewController: UIViewController {
         configureUI()
     }
     
-    // 기본적으로 0번째 항목의 내용을 출력. 6번째까지 있음 (+= 1 하자)
-    // 버튼을 누르면 출력되는 내용이 달라져야 함
-    
-    private func configureUI() {
-        loadBooks()
-        
-        view.backgroundColor = .systemBackground
-        
-        titleLabel.text = bookList[1 - 1].title
-        titleLabel.font = .systemFont(ofSize: 24, weight: .bold)
-        titleLabel.textAlignment = .center
-        titleLabel.numberOfLines = 2
-        
-        numberButton.setTitle("1", for: .normal)
-        numberButton.setTitleColor(.white, for: .normal)
-        numberButton.titleLabel?.font = .systemFont(ofSize: 16)
-        numberButton.backgroundColor = .systemBlue
-        numberButton.layer.cornerRadius = 20
-        
-        
-        
-        [titleLabel, numberButton]
-            .forEach {
-                view.addSubview($0)
-            }
-        
-        // leading, trailing = superView로부터 20
-        // top = safeArea로부터 10
-        titleLabel.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.width.equalTo(300)
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(10)
-        }
-        
-        numberButton.snp.makeConstraints {
-            $0.top.equalTo(titleLabel.snp.bottom).offset(16)
-            $0.centerX.equalToSuperview()
-            $0.width.height.equalTo(40)
-            
-        }
-    }
+    private var bookList: [Book] = []
     
     private let dataService = DataService()
     
@@ -79,7 +35,112 @@ class ViewController: UIViewController {
             }
         }
     }
+    
+    // 기본적으로 0번째 항목의 내용을 출력. 6번째까지 있음 (+= 1 하자)
+    // 버튼을 누르면 출력되는 내용이 달라져야 함
+    
+    private func configureUI() {
+        view.backgroundColor = .systemBackground
+        
+        loadBooks()
+        
+        guard !bookList.isEmpty else {
+            return
+        }
+        
+        // 제목 라벨 설정
+        var titleLabel = UILabel()
+        titleLabel.text = bookList[0].title
+        titleLabel.font = .systemFont(ofSize: 24, weight: .bold)
+        titleLabel.textAlignment = .center
+        titleLabel.numberOfLines = 2
+        
+        // 시리즈 순서 버튼 설정
+        var numberButton = UIButton()
+        numberButton.setTitle("1", for: .normal)
+        numberButton.setTitleColor(.white, for: .normal)
+        numberButton.titleLabel?.font = .systemFont(ofSize: 16)
+        numberButton.backgroundColor = .systemBlue
+        numberButton.layer.cornerRadius = 20
+        
+        
+        // 책 표지 이미지 속성
+        var bookCoverImageView = UIImageView()
+        bookCoverImageView.image = UIImage(named: "harrypotter1.jpg")
+        bookCoverImageView.contentMode = .scaleAspectFit // 바꿔보기
+        bookCoverImageView.snp.makeConstraints {
+            $0.width.equalTo(100)
+            $0.height.equalTo(150)
+        }
+        
+        // 책 제목 속성
+        let bookTitleLabel = UILabel()
+        bookTitleLabel.text = bookList[0].title
+        bookTitleLabel.textColor = .black
+        bookTitleLabel.font = .systemFont(ofSize: 20, weight: .bold)
+        bookTitleLabel.textAlignment = .center
+        bookTitleLabel.numberOfLines = 2
+        
+        func createInfoRow(titleLabel: UILabel, valueLabel: UILabel) -> UIStackView {
+            let hStack = UIStackView(arrangedSubviews: [titleLabel, valueLabel])
+            hStack.axis = .horizontal
+            hStack.spacing = 8
+            hStack.alignment = .leading
+            return hStack
+        }
+        
+        func titleLabelMaker(text: String, fontSize: CGFloat) -> UILabel {
+            let titleLabel = UILabel()
+            titleLabel.text = text
+            titleLabel.textColor = .black
+            titleLabel.font = .systemFont(ofSize: fontSize, weight: .bold)
+            return titleLabel
+        }
+        
+        func valueLabelMaker(text: String, textColor: UIColor, fontSize: CGFloat) -> UILabel {
+            let valueLabel = UILabel()
+            valueLabel.text = text
+            valueLabel.textColor = textColor
+            valueLabel.font = .systemFont(ofSize: fontSize)
+            return valueLabel
+        }
+        
+        // 이 세 Row들 간의 간격은? StackView가 알아서 맞춰줌
+        let authorRow = createInfoRow(titleLabel: titleLabelMaker(text: "Author", fontSize: 16), valueLabel: valueLabelMaker(text: bookList[0].author, textColor: .darkGray, fontSize: 18))
+        let releaseDateRow = createInfoRow(titleLabel: titleLabelMaker(text: "Released", fontSize: 14), valueLabel: valueLabelMaker(text: dateFormatterHelper(bookList[0].release_date), textColor: .gray, fontSize: 16))
+        let pagesRow = createInfoRow(titleLabel: titleLabelMaker(text: "Pages", fontSize: 14), valueLabel: valueLabelMaker(text: "\(bookList[0].pages)", textColor: .gray, fontSize: 14))
+        
+        let textStackView = UIStackView(arrangedSubviews: [bookTitleLabel, authorRow, releaseDateRow, pagesRow])
+        textStackView.axis = .vertical
+        textStackView.spacing = 8
+        textStackView.alignment = .leading
+        
+        let bookInfoStackView = UIStackView(arrangedSubviews: [bookCoverImageView, textStackView])
+        bookInfoStackView.axis = .horizontal
+        bookInfoStackView.spacing = 16
+        bookInfoStackView.alignment = .top
+        
+        [titleLabel, numberButton, bookInfoStackView]
+            .forEach {
+                view.addSubview($0)
+            }
+        
+        titleLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(300)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(10)
+        }
+        
+        numberButton.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(16)
+            $0.centerX.equalToSuperview()
+            $0.width.height.equalTo(40)
+        }
+        
+        bookInfoStackView.snp.makeConstraints {
+            $0.top.equalTo(numberButton.snp.bottom).offset(20)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+    
+    }
 }
-
-
-
